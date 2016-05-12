161aed6440d1fd2a8b0f9af5dd92ff92c49000bf
Brent Kaplan <bkaplan4@ku.edu>
2016-05-11 22:50:39 -0500

    initial commit of example data

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' Example alcohol purchase task data
+#'
+#' A dataset containing alcohol purchase task data for a small number of participants
+#'
+#' @format A matrix with price in the first column and participant responses in subsequent columns. Participants were asked how many standard sized alcoholic beverages they would buy at various prices.
+"apt"
