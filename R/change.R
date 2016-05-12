d1a16ae8206d9fdfd27aa5836bc404d64d2f506f
Brent Kaplan <bkaplan4@ku.edu>
2016-05-12 13:44:16 -0500

    add copyright

--- R/change.R
+++ R/change.R
@@ -1,3 +1,26 @@
+##
+## Copyright 2016 Brent Kaplan
+##
+## This file is part of beezdemand.
+##
+## beezdemand is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, version 2.
+##
+## beezdemand is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with beezdemand.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>.
+##
+## summary
+## R script for changing values and splitting data
+##
+##
+##
+
 ##' Changes 0 values or applies a parallel shift.
 ##'
 ##' Takes a matrix of consumption values and replaces specified number of 0s with replacement value. Optionally, can apply a parallel shift to all consumption values and prices.
