c298de53bfb7501f6dc8b5ce5ea5203855c6659b
Brent Kaplan <bkaplan4@ku.edu>
2016-05-12 13:52:24 -0500

    update copyright

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,29 @@
+##
+## Copyright 2016 Brent Kaplan
+##
+## This file is part of beezdemand.
+##
+## beezdemand is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, version 2.
+##
+## beezdemand is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with beezdemand.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>.
+##
+## summary
+## R script for utility functions
+##
+##createOutDir derived from
+## @kutils = functions used in the CRMDA (Copyright - 2016 - Paul Johnson - GPLv2)
+##
+##
+##
+
 ##' Manufacture an output directory that is unique
 ##'
 ##' Creates out directory and if proposed new directory already
