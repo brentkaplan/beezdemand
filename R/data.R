df6db30ac872d6a4248a5453793d4134b557ea54
Brent Kaplan <bkaplan4@ku.edu>
2016-05-12 13:56:40 -0500

    update copyright

--- R/data.R
+++ R/data.R
@@ -1,3 +1,26 @@
+##
+## Copyright 2016 Brent Kaplan
+##
+## This file is part of beezdemand.
+##
+## beezdemand is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, version 2.
+##
+## beezdemand is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with beezdemand.  If not, see <http://www.gnu.org/licenses/gpl-2.0.html>.
+##
+## summary
+## Data used in examples
+##
+##
+##
+
 #' Example alcohol purchase task data
 #'
 #' A dataset containing alcohol purchase task data for a small number of participants
