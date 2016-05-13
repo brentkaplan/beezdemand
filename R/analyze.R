1eca83ef6a90fb5ac4456a72c7ca2dd28bacccf8
Brent Kaplan <bkaplan4@ku.edu>
2016-05-12 21:12:23 -0500

    doEverything stops if less than 3 datapoints or no consumption

--- R/analyze.R
+++ R/analyze.R
@@ -146,8 +146,8 @@ doEverything <- function(mat, prices, include0 = TRUE, equation, optimizer, k, r
 
     f <- function(adf, equation, optimizer, k, remq0e) {
         if (is.character(adf)) {
-            if (adf == "Fewer than 3 datapoints") stop("I can not and will not analyze a dataset with less than 3 positive datapoints")
-            if (adf == "No consumption") stop("I can not and will not analyze a dataset with no consumption")
+            if (adf == "Fewer than 3 datapoints") stop("I can not and will not analyze a dataset with less than 3 positive datapoints. Please run your dataset through systemCheck.")
+            if (adf == "No consumption") stop("I can not and will not analyze a dataset with no consumption. . Please run your dataset through systemCheck.")
         }
         q0e <- adf[1, "y"]
         adf <- if (remq0e) adf[-1,] else adf
