d495d1e73f922bee264efcc282e4e8ebccb2feeb
Brent Kaplan <bkaplan4@ku.edu>
2016-05-15 14:18:59 -0500

    ReplaceZeros for long form dataframe

--- R/change.R
+++ R/change.R
@@ -21,6 +21,52 @@
 ##
 ##
 
+##' Replaces 0 values
+##'
+##' Replaces specified number of 0s with replacement value.
+##' @title Replace Zeros
+##' @param dat Dataframe (long form)
+##' @param nrepl Number of zeros to replace with replacement value (replnum). Can accept either a number or "all" if all zeros should be replaced. Default is to replace the first zero only.
+##' @param replnum Value to replace zeros. Default is .01
+##' @return Dataframe (long form)
+##' @author Brent Kaplan <bkaplan4@@ku.edu>
+##' @examples
+##' ReplaceZeros(apt)
+##' ## Replace all zeros with .01
+##' ReplaceZeros(apt, nrepl = "all", replnum = .01)
+##' @export
+ReplaceZeros <- function(dat, nrepl = 1, replnum = .01) {
+
+    ## Get N unique participants, informing loop
+    participants <- unique(dat$id)
+    np <- length(participants)
+
+    for (i in seq_len(np)) {
+
+        adf <- NULL
+        adf <- dat[dat$id == participants[i], ]
+
+        if (!any(adf$y == 0)) {
+            dat[dat$id == participants[i], ] <- adf
+            next
+        } else {
+            if (nrepl == "all") {
+                ind <- which(adf$y == 0, arr.ind = TRUE)
+                adf$y[ind] <- replnum
+                dat[dat$id == participants[i], ] <- adf
+            } else {
+                ind <- which(adf$y == 0, arr.ind = TRUE)
+                nrep <- if (length(ind) < nrepl) length(ind) else nrepl
+                adf$y[ind[1:nrep]] <- replnum
+                dat[dat$id == participants[i], ] <- adf
+            }
+        }
+    }
+    dat
+}
+
+
+
 ##' Changes 0 values or applies a parallel shift.
 ##'
 ##' Takes a matrix of consumption values and replaces specified number of 0s with replacement value. Optionally, can apply a parallel shift to all consumption values and prices.
