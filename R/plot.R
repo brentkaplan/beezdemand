c20d94188fdaaeac6c76c8299b072bda427453f9
Brent Kaplan <bkaplan4@ku.edu>
2017-03-06 20:26:43 -0500

    plot.R: add APA theme for future graphs

--- R/plot.R
+++ R/plot.R
@@ -64,6 +64,38 @@ minTicks <- function(maj) {
     }
 }
 
+##' APA theme for ggplot
+##'
+##' Theme for ggplot graphics that closely align with APA formatting
+##' @title APA Theme
+##' @param plot.box Boolean for a box around the plot
+##' @return ggplot theme
+##' @author Brent Kaplan <bkaplan4@@ku.edu>
+theme_apa <- function(plot.box = FALSE){
+    helv <- "Helvetica"
+
+    out <- theme(
+        plot.title = element_text(family = helv, size = 14, face = "bold", colour = "black"),
+        axis.title.x = element_text(family = helv, size = 14, colour = "black"),
+        axis.title.y = element_text(family = helv, size = 14, angle = 90, colour = "black"),
+        axis.text.x = element_text(family = helv, size = 11, colour = "black"),
+        axis.text.y = element_text(family = helv, size = 11, colour = "black"),
+        axis.ticks = element_line(colour="black"))
+
+    if (plot.box) {
+        out <- out + theme(panel.background = element_rect(fill = "white",
+                colour = "black"), panel.border = element_rect(fill = NA,
+                colour = "white"), axis.line = element_line())
+    } else {
+        out <- out + theme(panel.background = element_blank(),
+                           panel.border = element_blank(),
+                           panel.grid.major = element_blank(),
+                           panel.grid.minor = element_blank(),
+                           axis.line = element_line(colour = "black"))
+    }
+    out
+}
+
 
 ##' Creates plots
 ##'
