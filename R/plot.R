d73702e84c2d55805da76b87eb3b4d4fcae46c3f
Brent Kaplan <bkaplan.ku@gmail.com>
2017-08-29 10:52:15 -0400

    importFrom layer

--- R/plot.R
+++ R/plot.R
@@ -83,6 +83,7 @@ minTicks <- function(maj) {
 ##' @param ... additional arguments
 ##' @return ggplot2 layer
 ##' @author Shawn Gilroy <shawn.gilroy@@temple.edu>
+##' @importFrom ggplot2 layer
 ##' @export
 annotation_logticks2 <- function(base = 10, sides = "bl", scaled = TRUE, short = unit(0.1, "cm"),
                                  mid = unit(0.2, "cm"), long = unit(0.3, "cm"), colour = "black",
